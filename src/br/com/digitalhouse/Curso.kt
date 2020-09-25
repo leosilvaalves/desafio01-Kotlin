98bd60038ad14e7cfa3d6ebc544bb8ae3dd3def6
leosilvaalves <leo_silvaalves@yahoo.com.br>
2020-09-25 16:32:42 -0300

    informacoes sobre qtdade de alunos e professores adicionadas na classe curso

--- src/br/com/digitalhouse/Curso.kt
+++ src/br/com/digitalhouse/Curso.kt
@@ -3,7 +3,10 @@ package br.com.digitalhouse
 class Curso {
     val nome:String? = null
     val codigoDeCurso:Int? = null
-
+    val profTitutar:ProfTitutar?= null
+    val profAdjunto: ProfAdjunto?= null
+    val qtdeMaximaAlunos:Int=0
+    val alunosMatriculados: MutableList <Aluno> = mutableListOf()
 
     override fun equals(other: Any?): Boolean {
         return other is Curso && this.codigoDeCurso==other.codigoDeCurso
